24f4e5d167ff598bec452eb6fdcc518458d3c344
leoliu012 <113882080+leoliu012@users.noreply.github.com>
2026-08-31 23:05:54 -0400

    Add: car-fleet - submission-22

---
+++ Data Structures & Algorithms/car-fleet/submission-22.py
@@ -0,0 +1,43 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        ordered = []
+        for i in range(len(position)):
+            ordered.append([position[i],speed[i]])
+        ordered.sort()
+        
+        hours_takes = []
+        for pos,s in ordered:
+            remain = (target-pos)
+            hours_takes.append(remain/s)
+        
+        ret = 1
+        # in_fleet = False
+        # for i in range(len(hours_takes)-1):
+        #     if hours_takes[i] > hours_takes[i+1]:
+        #         in_fleet = False
+        #         ret += 1
+        #     else:
+        #         if not in_fleet:
+        #             ret += 1
+        # #             in_fleet = True
+        print(len(hours_takes))
+        res = set([hours_takes[-1]])
+        if len(hours_takes) > 1:
+            ind = len(hours_takes)-2
+            while ind >= 0:
+                if hours_takes[ind] > hours_takes[ind+1]:
+                    # print(hours_takes[ind-1], hours_takes[ind])
+                    res.add(hours_takes[ind])
+                    ind -= 1
+                else:
+                    res.add(hours_takes[ind+1])
+                    # print(ind-1, hours_takes[ind-1], hours_takes[ind])
+                    slow = ind+1
+                    while ind >= 0 and hours_takes[ind] <= hours_takes[slow]:
+                        ind -= 1
+        else:
+            return 1
+
+        print(res)
+        return len(res)
+            
