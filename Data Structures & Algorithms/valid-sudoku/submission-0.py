b00ac8690edcd638247501fb123966a74417107c
leoliu012 <113882080+leoliu012@users.noreply.github.com>
2026-08-31 20:17:17 -0400

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.py
@@ -0,0 +1,43 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        row_num = len(board)
+        col_num = len(board[0])
+        if (not row_num == 9) or (not col_num == 9):
+            print(row_num, col_num)
+            return False
+        it = 0
+        for row in range(row_num):
+            row_set = set()
+            for col in range(col_num):
+                if not board[row][col] == '.':
+                    curr = int(board[row][col])
+                    if not curr >= 1 and curr <= 9:
+                        print("0")
+                        return False
+                    if curr in row_set:
+                        print("1")
+                        return False
+                    row_set.add(curr)
+        
+        for col in range(col_num):
+            col_set = set()
+            for row in range(row_num):
+                if not board[row][col] == '.':
+                    curr = int(board[row][col])
+                    if curr in col_set:
+                        print(curr, row_set)
+                        return False
+                    col_set.add(curr)
+        
+        for it_row in range(row_num//3):
+            for it_col in range(row_num//3):
+                square_set = set()
+                for row in range(3):
+                    for col in range(3):
+                        if not board[row+it_row*3][col+it_col*3] == '.':
+                            curr = int(board[row+it_row*3][col+it_col*3])
+                            if curr in square_set:
+                                print("3")
+                                return False
+                            square_set.add(curr)
+        return True
