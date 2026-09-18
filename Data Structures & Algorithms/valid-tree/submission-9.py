bd637800baddb583889e751d0563f182ac807d7a
leoliu012 <113882080+leoliu012@users.noreply.github.com>
2026-09-18 17:09:31 -0400

    Add: valid-tree - submission-9

---
+++ Data Structures & Algorithms/valid-tree/submission-9.py
@@ -0,0 +1,26 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        graph = [[] for _ in range(n)]
+
+        for edge in edges:
+            n1 = edge[0]
+            n2 = edge[1]
+            graph[n1].append(n2)
+            graph[n2].append(n1)
+        
+
+        visited = set()
+
+        q = deque([(0,-1)])
+        while q:
+            node, parent = q.popleft()
+            if node in visited:
+                return False
+            visited.add(node)
+            new_nodes = graph[node]
+            for new_node in new_nodes:
+                if new_node == parent:
+                    continue
+                q.append((new_node, node))
+
+        return len(visited) == n
